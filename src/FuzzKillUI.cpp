7ca42ff2c7125b5d0a9339af34f986235caaa022
Kyn21kx <nefesmeralda@gmail.com>
2025-09-30 11:37:30 -0600

    Finished first calc version

--- src/FuzzKillUI.cpp
+++ src/FuzzKillUI.cpp
@@ -50,6 +50,7 @@ void FuzzKillUI::Init() {
 	this->m_filteredProcesses.clear();
 	this->m_activeProcesses.clear();
 	this->m_activeProcessesNames.clear();
+	std::memset(this->m_operationResultStr, 0, MAX_OPERATION_RESULT_COUNT);
 	// Gather all the processes here from the windows API
 	EError err = ProcessLayer::FetchProcessesInto(&this->m_activeProcesses);
 	if (err != EError::Ok) {
@@ -96,6 +97,9 @@ void FuzzKillUI::DrawUI() {
     CLAY({.id = CLAY_ID("MainContainer"), .layout = { .sizing = SIZE_AUTO_GROW_XY, .layoutDirection = CLAY_TOP_TO_BOTTOM }, .backgroundColor = ColorUtils::White(BG_COLOR_ALPHA * 2)}) {
     	bool isPlaceholder = this->m_query.empty();
     	Clay_String headerText = isPlaceholder ? CLAY_STRING("Search for any running application...") :StrToClayString(this->m_query.c_str(), this->m_query.size());
+        if (this->m_query.empty() && strlen(this->m_operationResultStr) > 0) {
+        	headerText = StrToClayString(this->m_operationResultStr, strlen(this->m_operationResultStr));
+        }
         CLAY_TEXT(headerText, CLAY_TEXT_CONFIG(DefaultText(72)));
         for (size_t i = 0; i < this->m_filteredProcesses.size(); i++) {
         	const WinProcess& process = this->m_activeProcesses[this->m_filteredProcesses[i]];
@@ -116,6 +120,8 @@ void FuzzKillUI::ResetFilterIfNeeded() {
 void FuzzKillUI::HandleKeyboardInput(float delta) {
 	int32_t c = GetCharPressed();
 	if (c > 0) {
+		// Reset the operation result string here
+		std::memset(this->m_operationResultStr, 0, MAX_OPERATION_RESULT_COUNT);
 		this->m_query += c;
 		this->m_filteredProcesses = FuzzyFindIndices<std::vector<std::string_view>, std::string_view>(this->m_activeProcessesNames, this->m_query);
 		this->selectedProcess = 0;
@@ -175,15 +181,54 @@ EError ParseOperation(const std::string& expression, float* outResult) {
                 float opResult = MathUtils::ApplyOperation(a, b, op);
                 numbers.push(opResult);
 			}
+			operators.push(static_cast<MathUtils::OperationType>(token[0]));
 			continue;
 		}
+		if (token[0] == '(') {
+			// This enum entry does not really exist, but we'll just trust it
+			operators.push(static_cast<MathUtils::OperationType>('('));
+		}
+		else if (token[0] == ')') {
+			while (!operators.empty() && (char)operators.top() != '(') {
+				// Pop two numbers and one operator
+                float b = numbers.top();
+                numbers.pop();
+                float a = numbers.top();
+                numbers.pop();
+                MathUtils::OperationType op = operators.top();
+                operators.pop();
+                // Apply the operator to the numbers and
+                // push the result onto the operand stack
+                float result = MathUtils::ApplyOperation(a, b, op);
+                numbers.push(result);
+				
+			}
+			// Pop the parenthesis
+			operators.pop();
+		}
 	}
-
 
 	if (operators.empty() || numbers.size() < 2) {
 		// Not enough operators or operands to execute an action
 		return EError::ParseFailed;
 	}
+	
+    // While the operator stack is not empty
+    while (!operators.empty()) {
+        // Pop two numbers and one operator
+        float b = numbers.top();
+        numbers.pop();
+        float a = numbers.top();
+        numbers.pop();
+        MathUtils::OperationType op = operators.top();
+        operators.pop();
+        // Apply the operator to the numbers and push the
+        // result onto the operand stack
+        float result = MathUtils::ApplyOperation(a, b, op);
+        numbers.push(result);
+    }
+
+    *outResult = numbers.top();
 
 	return EError::Ok;
 }
@@ -193,7 +238,8 @@ void FuzzKillUI::OnTextSubmit() {
 	float operationResult;
 	EError operationErr = ParseOperation(this->m_query, &operationResult);
 	if (operationErr == EError::Ok) { // It WAS a math expression, let's update the UI accordingly
-
+		this->m_query = "";
+		std::snprintf(this->m_operationResultStr, MAX_OPERATION_RESULT_COUNT, "%f", operationResult);
 		return;
 	}
 
@@ -224,3 +270,20 @@ void FuzzKillUI::DrawProcessListItem(const WinProcess& processInfo, int32_t inde
 	
 }
 
+
+void FuzzKillUI::DrawListContainer(const std::string_view& text) {
+	const Clay_LayoutConfig layoutConfig = {
+		.sizing = SIZE_AUTO_GROW_XY
+	};
+	const Clay_BorderElementConfig borderConfig = {
+		.color = ColorUtils::Red()
+	};
+	constexpr Clay_Color inactiveColor = ColorUtils::White(BG_COLOR_ALPHA * 2);
+	constexpr Clay_Color activeColor = ColorUtils::Red(BG_COLOR_ALPHA * 2);
+
+	Clay_String nameStr = StrToClayString(text.data(), text.size());
+	// TODO: Fix the id here
+	CLAY({ .id = CLAY_IDI("ListContainer", 0), .layout = layoutConfig, .backgroundColor = inactiveColor, .border = borderConfig}) {
+	    CLAY_TEXT(nameStr, CLAY_TEXT_CONFIG(DefaultText(32)));
+	}
+}
